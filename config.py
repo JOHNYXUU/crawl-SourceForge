83fcbd521062200a17b4b18f35a0ababf680f243
JOHNYXUU <1163444531@qq.com>
2019-05-14 17:48:05 +0800

    add note in Chinese

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-url = "https://sourceforge.net/directory/language:java/os:linux/"
+url = "https://sourceforge.net/directory/language:java/os:linux/"#可以修改（注：最好该分类下的项目数目不要太大，建议该分类下的English分类的数目小于25000，因为英文是最多的）
 item_index_head = 'https://sourceforge.net'
