ed2c20b13ffc03122338838b837d8770e27ce380
JOHNYXUU <1163444531@qq.com>
2019-05-14 18:38:56 +0800

    add dir_name in config

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 url = "https://sourceforge.net/directory/language:java/os:linux/"#可以修改（注：最好该分类下的项目数目不要太大，建议该分类下的English分类的数目小于25000，因为英文是最多的）
 item_index_head = 'https://sourceforge.net'
+dir_name = '/root/SourceForgelinxjava/data/'#改成你的目录
