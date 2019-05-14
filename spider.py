6c77c0321dd86ae15ab349f1a1a8740d1994fda3
JOHNYXUU <1163444531@qq.com>
2019-05-14 17:47:47 +0800

    add some note in Chinese

--- spider.py
+++ spider.py
@@ -20,17 +20,17 @@ def get_page_text(url): #用于获取页面HTML代码的text
         print('ERROR when requesting this page {}'.format(url))#假如无法请求，报错，同时输出错误网页的网址
         return  None
 
-def get_translation_type(html):#用于获得
-    url_heads = []
+def get_translation_type(html):#用于获得翻译语言的类型，和原始的url组成新的url_head，为的是处理项目数目过多而导致的999页后无法浏览
+    url_heads = []#存放所有的url_head
     doc = pq(html)
     items = doc.find('#facet-natlanguage li').items()
     for item in items:
         if item.text() != 'More...':
-            if item.text().split(' ')[1] == '(Simplified)':
+            if item.text().split(' ')[1] == '(Simplified)':#简体中文
                 type = item.text().split(' ')[0].lower() + 'simplified'
-            elif item.text().split(' ')[1] == '(Traditional)':
+            elif item.text().split(' ')[1] == '(Traditional)':#繁体中文
                 type = item.text().split(' ')[0].lower() + 'traditional'
-            elif 'Brazilian Portuguese' in item.text():
+            elif 'Brazilian Portuguese' in item.text():#以下四个较为特殊，因此单独处理
                 type = 'portuguesebrazilian'
             elif 'Slovene' in item.text():
                 type = 'slovenian'
@@ -38,30 +38,28 @@ def get_translation_type(html):#用于获得
                 type = 'irish_gaelic'
             elif 'Scottish Gaelic' in item.text():
                 type = 'scottish-gaelic'
-            elif item.text().split(' ')[1][0] != "(":
-                type = item.text().split(' ')[0].lower() + '-' + item.text().split(' ')[1].lower()
-            else:
+            else:#一般情况，仅把语言名称换成小写即可
                 type = item.text().split(' ')[0].lower()
             translation.append(type)
-            url_heads.append("https://sourceforge.net/directory/natlanguage:{}/language:java/os:linux/".format(type))
+            url_heads.append("https://sourceforge.net/directory/natlanguage:{}/language:java/os:linux/".format(type))#和原始的url组成新的url_head，假如找别的分类，这个也要修改成合适的url
     return url_heads
 
-def get_page_num(url_head):
+def get_page_num(url_head):#利用二分法求这种翻译语言下的项目共有几页
     left = 1
-    right = 999
+    right = 999#999为上限，因为999页过后不能浏览，所以假如项目的分类不精确，该爬虫爬取信息将不完整
     mid = (left + right)//2
     while(left<right):
         url = url_head + '?page={}'.format(mid)
         html = get_page_text(url)
         doc = pq(html)
-        if 'No results found.' in doc.find('.project_info').text():
+        if 'No results found.' in doc.find('.project_info').text():#No results found.为超出页数范围的标志
             right = mid - 1
         else:
             left = mid + 1
         mid = (left + right)//2
     return mid-1
 
-def get_item_index(html):
+def get_item_index(html):#获得某个项目的网址
     doc = pq(html)
     items = doc('#pg_directory .off-canvas-content .l-two-column-page .l-content-column .m-project-search-results li').items()
     for item in items:
@@ -70,7 +68,7 @@ def get_item_index(html):
             index = item_index_head + index_tail
             yield index
 
-def get_item_user_ratings(doc):
+def get_item_user_ratings(doc):#获得项目的星级评价，包括平均星数和每个级别有几个人给
     dimensional_ratings = doc('.dimensional-ratings .dimensional-rating').items()
     user_ratings = {
         'average': doc('.rating .average').text(),
@@ -80,12 +78,12 @@ def get_item_user_ratings(doc):
         'star_2': doc('.stars-2 .rating-label').text(),
         'star_1': doc('.stars-1 .rating-label').text()
     }
-    for dimensional_rating in dimensional_ratings:
+    for dimensional_rating in dimensional_ratings:#获得项目的dimensional_rating，就是项目网页中星级评价右边那个
         type = dimensional_rating.find('.label').text()
         user_ratings[type] = dimensional_rating.find('.rating-score .dim-rate').text() + '/5'
     return  user_ratings
 
-def cnt_review_stars(user):
+def cnt_review_stars(user):#计算某个用户给这个项目打了几颗星
     star_num = 0
     stars = user.find('.star').items()
     for star in stars:
@@ -93,7 +91,7 @@ def cnt_review_stars(user):
             star_num += 1
     return  star_num
 
-def get_user_reviews(index):
+def get_user_reviews(index):#获得用户的评价，汇总了上面两个评价种类
     page = 1
     no_name_num = 1
     users_reviews = {}
@@ -106,21 +104,21 @@ def get_user_reviews(index):
         doc = pq(html)
         stop_sign1 = doc.find('.info-no-reviews').text()
         stop_sign2 = (doc.find('.main-content .row p').text() == 'This project does not allow reviews to be posted.')
-        if stop_sign1 or stop_sign2:
+        if stop_sign1 or stop_sign2:#翻页停止的标志，1是指有评论且翻到头，2是指压根就没人评论这个项目
             break
         else:
             users = doc.find('#project-reviews li').items()
             for user in users:
                 user_name = user.find('.footer .author-name').text()
                 if user_name:
-                    if user_name == '<REDACTED>':
+                    if user_name == '<REDACTED>':#假如匿名评价，为此加上个数，来区分
                         user_name += '_{}'.format(no_name_num)
                         no_name_num += 1
                     review_infos = {}
                     review_infos['stars'] = cnt_review_stars(user)
-                    review_infos['date'] = user.find('.footer .created-date').text()[7:]
-                    review_infos['review_txt'] = user.find('.review-txt-outer .review-txt').text()
-                    helpful_cnt = user.find('.helpful-count .user-count').text()
+                    review_infos['date'] = user.find('.footer .created-date').text()[7:]#评价日期
+                    review_infos['review_txt'] = user.find('.review-txt-outer .review-txt').text()#评价内容
+                    helpful_cnt = user.find('.helpful-count .user-count').text()#认为该评价有用的人数
                     if helpful_cnt:
                         review_infos['help-count'] = helpful_cnt
                     else:
@@ -129,18 +127,18 @@ def get_user_reviews(index):
         page += 1
     return  users_reviews
 
-def get_item_information(item_html,index):
+def get_item_information(item_html,index):#获得项目所有信息的总框架
     doc = pq(item_html)
-    name = doc('.title h1').text()
-    summary = doc('.main-content .description').text()
+    name = doc('.title h1').text()#名称
+    summary = doc('.main-content .description').text()#总结
     summary = summary.replace('\n','')
-    user_ratings = get_item_user_ratings(doc)
+    user_ratings = get_item_user_ratings(doc)#用户评价
     try:
-        Categories = doc('.main-content .row.psp-section .small-12.medium-5.columns').text().split('\n')[1].replace('License','')
+        Categories = doc('.main-content .row.psp-section .small-12.medium-5.columns').text().split('\n')[1].replace('License','')#项目的分类，可能没有
     except:
         Categories = ''
     try:
-        License = doc('.main-content .row.psp-section .small-12.medium-5.columns').text().split('\n')[2]
+        License = doc('.main-content .row.psp-section .small-12.medium-5.columns').text().split('\n')[2]#项目的License，也可能没有
     except:
         License = ''
     item_infos = {
@@ -153,20 +151,20 @@ def get_item_information(item_html,index):
     }
     return item_infos
 
-def write_to_file(infos,url_head):
+def write_to_file(infos,url_head):#写入本地文件
     trans = ''
     for type in translation:
         if type in url_head:
             trans += type
             break
-    dir_name = '/root/SourceForgelinxjava/data/{}'.format(trans)
+    dir_name = '/root/SourceForgelinxjava/data/{}'.format(trans)#改成你的文件目录（注意这是以项目的翻译语言分类存储的）
     if not os.path.isdir(dir_name):
         os.makedirs(dir_name)
-    with open(dir_name+'/data.txt','a',encoding='utf-8') as f:
+    with open(dir_name+'/data.txt','a',encoding='utf-8') as f:#存储为txt文件
         f.write(json.dumps(infos,ensure_ascii=False)+'\n')
         f.close()
 
-def main_process(page,url_head):
+def main_process(page,url_head):#获得分类网址后的主要过程
     url = url_head + '?page={}'.format(page)
     html = get_page_text(url)
     indexs = get_item_index(html)
@@ -176,16 +174,16 @@ def main_process(page,url_head):
         write_to_file(infors,url_head)
 
 def main():
-    start = perf_counter()
+    start = perf_counter()#用于计算时间的花费
     html = get_page_text(url)
     url_heads = get_translation_type(html)
-    for url_head in url_heads:
+    for url_head in url_heads:#对于每一个分类，一页一页的爬
         page = get_page_num(url_head)
         for i in range(page):
             main_process(i+1,url_head)
-    end = perf_counter()
-    time_consumed = end - start
-    print(time_consumed)
+    end = perf_counter()#用于计算时间的花费
+    time_consumed = end - start#用于计算时间的花费
+    print(time_consumed)#用于计算时间的花费
     with open('/root/SourceForgelinxjava/time.txt','a',encoding='utf-8') as f:
         f.write(time_consumed)
         f.close()
