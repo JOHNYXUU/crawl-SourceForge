6cca17fde0ef5338cca28d3913f367eec84b1e3c
JOHNYXUU <1163444531@qq.com>
2019-05-16 08:56:30 +0800

    add end sentence

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 url = "https://sourceforge.net/directory/language:java/os:linux/"#可以修改（注：最好该分类下的项目数目不要太大，建议该分类下的English分类的数目小于25000，因为英文是最多的）
 item_index_head = 'https://sourceforge.net'
 dir_name = '/root/SourceForgelinxjava/data/'#改成你的目录
+happy_end = "all works are done!\n" \
+            "please remember to check whether any pages went into error in file timeanderror" \
+            "you can also check the time this project takes in file timeanderror"
