5606f922e3f8667d03767b5d761a862933c4fc43
JOHNYXUU <1163444531@qq.com>
2019-05-21 19:00:53 +0800

    add mysql config and 3 tables' code

--- config.py
+++ config.py
@@ -5,7 +5,35 @@ happy_end = "all works are done!\n" \
             "please remember to check whether any pages went into error in file timeanderror" \
             "you can also check the time this project takes in file timeanderror"
 
-mysql_proxy = ''
+mysql_host = ''
 mysql_username = ''
 mysql_passwords = ''
 mysql_database = ''
+
+table_1 = "CREATE TABLE `briefintro` (`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id of items'," \
+          "`name` varchar(50) DEFAULT NULL COMMENT 'name of items'," \
+          "`summary` text COMMENT 'summary of items'," \
+          "`Categories` tinytext COMMENT 'categories of items'," \
+          "`Licenses` tinytext COMMENT 'Licenses of items'," \
+          " PRIMARY KEY (`id`)" \
+          ") ENGINE=InnoDB AUTO_INCREMENT=9376 DEFAULT CHARSET=utf8mb4 COMMENT='briefintro about java&Linux on SourceForge';"
+table_2 = "CREATE TABLE `userratings` (" \
+          "`Id` int(11) NOT NULL AUTO_INCREMENT," \
+          "`average` varchar(50) DEFAULT NULL," \
+          "`num_of_5stars` varchar(50) DEFAULT NULL," \
+          "`num_of_4stars` varchar(50) DEFAULT NULL," \
+          "`num_of_3stars` varchar(50) DEFAULT NULL," \
+          "`num_of_2stars` varchar(50) DEFAULT NULL," \
+          "`num_of_1star` varchar(50) DEFAULT NULL," \
+          "`Ease` varchar(50) DEFAULT NULL," \
+          "`Features` varchar(50) DEFAULT NULL," \
+          "`Design` varchar(50) DEFAULT NULL," \
+          "`Support` varchar(50) DEFAULT NULL," \
+          " PRIMARY KEY (`Id`)) ENGINE=InnoDB AUTO_INCREMENT=9516 DEFAULT CHARSET=utf8mb4;"
+table_3 = "CREATE TABLE `userreviews` (" \
+          "`id` int(11) DEFAULT NULL," \
+          "`nickname` varchar(50) DEFAULT NULL," \
+          "`num_of_stars` int(11) DEFAULT NULL," \
+          "`date` varchar(50) DEFAULT NULL," \
+          "`review_txt` text," \
+          "`helpful_cnt` varchar(50) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
